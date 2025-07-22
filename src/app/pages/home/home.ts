77c00ba411cbc4f50e2340d3ccdf59020edcf4ab
Miguel P <87776778+MiguelParraC@users.noreply.github.com>
2025-07-22 10:18:39 -0600

    tada: Aplicación de ToDo en Angular

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,136 @@
+import { Component, computed, effect, inject, Injector, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Task } from '../../models/task.model';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+  task = signal<Task[]>([]);
+
+
+  filter = signal('all');
+  tasksByFilter = computed(() => {
+    const filter = this.filter();
+    const tasks = this.task();
+    if (filter === 'all') {
+      return tasks;
+    } else if (filter === 'completed') {
+      return tasks.filter(task => task.completed);
+    } else if (filter === 'pending') {
+      return tasks.filter(task => !task.completed);
+    }
+    return tasks;
+  });
+
+
+  injector = inject(Injector)
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      this.task.set(JSON.parse(storedTasks));
+    }
+    this.trackTask();
+  }
+
+  trackTask() {
+    effect(() => {
+      const tasks = this.task();
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    },{ injector: this.injector });
+  }
+
+  newTaskCtrl = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+    ],
+  });
+
+  changeHandler() {
+    if (this.newTaskCtrl.valid && this.newTaskCtrl.value.trim()) {
+      const value = this.newTaskCtrl.value.trim();
+      this.addTask(value);
+      this.newTaskCtrl.setValue('');
+    }
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title: title,
+      completed: false,
+    }
+    this.task.update((task) => [...task, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.task.update((task) => task.filter((task, position) => position !== index));
+  }
+
+  updateTask(index: number) {
+    this.task.update((task) => {
+      const updatedTask = { ...task[index] };
+      return task.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            completed: !task.completed,
+          };
+        }
+        return task;
+      });
+    });
+  }
+
+  editTask(index: number) {
+    this.task.update(prevState => {
+      const updatedTask = { ...prevState[index], editing: true };
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            editing: true
+          };
+        }
+        return {
+          ...task,
+          editing: false
+        }
+      });
+    });
+  }
+
+  updateText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value.trim();
+    this.task.update((task) => {
+      const updatedTask = { ...task[index] };
+      return task.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            title: newValue,
+            editing: false,
+          };
+        }
+        return task;
+      });
+    });
+  }
+
+  changeFilter(filter: string) {
+    this.filter.set(filter);
+  }
+
+}
---
+++ src/app/pages/labs/labs.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-labs',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './labs.html',
+  styleUrl: './labs.css'
+})
+export class Labs {
+  protected readonly welcome = signal('Hola Mundo!');
+
+  task = signal([
+    'Instalar Angular',
+    'Tarea 2',
+    'Tarea 3',
+  ]);
+
+  name = signal('Miguel');
+  edad = 36;
+  disabled = false;
+
+  colorCtrl = new FormControl();
+  widthCtrl = new FormControl(50, {
+    nonNullable: true,
+
+  });
+
+  nameCtrl = new FormControl("miguel", {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(10),
+    ]
+  });
+
+
+  constructor() {
+    this.colorCtrl.valueChanges.subscribe( value => {
+      console.log(value);
+    });
+  }
+
+  clickHandler() {
+    alert('Hola Mundo!');
+  }
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+
+  }
+  keydownHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
+   
+}
